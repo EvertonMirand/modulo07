49eb6a5a543f00e0c42906290b31b0a33f30ba53
Everton Miranda Vitório <c1300290@interno.bb.com.br>
2019-09-10 10:41:07 -0300

    Continuando o modulo 07

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -4,14 +4,26 @@ import { addToCartSucess, updateAmount } from './actions';
 import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
   const productExists = yield select(state =>
     state.cart.find(p => p.id === id)
   );
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERROR');
+    return;
+  }
+
   if (productExists) {
-    const amount = productExists.amount + 1;
     yield put(updateAmount(id, amount));
   } else {
+    const response = yield call(api.get, `/products/${id}`);
     const data = {
       ...response.data,
       amount: 1,
