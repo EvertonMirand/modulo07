e18b917f1ef08039d1df6f6cf12537457bf32af7
Everton Miranda Vitório <c1300290@interno.bb.com.br>
2019-09-10 11:05:57 -0300

    Continuando o modulo 07

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,7 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
-import { addToCartSucess, updateAmount } from './actions';
+import { addToCartSucess, updateAmountSucess } from './actions';
 import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
@@ -23,7 +23,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSucess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
     const data = {
@@ -36,4 +36,18 @@ function* addToCart({ id }) {
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora do estoque');
+    return;
+  }
+  yield put(updateAmountSucess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
